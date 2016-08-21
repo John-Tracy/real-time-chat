8b461add832ef51ded56d63aca19b9662b3f30a8
John Tracy <johnmtracy133@hotmail.com>
2016-08-21 16:04:01 -0400

    multi chat capability w/ nicknames

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 
-// require express sessions
-session = require('express-session');
+// require mongodb
+var mongojs = require('mongojs');
+// Database configuration
+var databaseUrl = 'real-time';
+var collections = ["user"];
+//  mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on('error', function(err) {
+  console.log('Database Error:', err);
+});
+// socket namespace constructor
+function NameSpace(name){
+	this.name = name;
+};
+
 // init express server
 var app = require('express')();
-// adds sessions to app
-app.use(session({
-    secret: 'no secret',
-    resave: true,
-    saveUninitialized: true
-}));
+// Sets up the Express app to handle data parsing 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
 // inits http to the server
 var http = require('http').Server(app);
 // requires socket.io and syncs it with http
@@ -17,26 +31,39 @@ var path = require('path');
 
 
 // routing for index.html
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.post('/login', function(req, res){
 
+		db.user.find({"name": req.body.user}, function(err, docs){
+		if(docs != null || undefined){
+			res.json({
+					name: docs[0].name,
+					message: "success"
+			})
+		}
+		else{
+			res.json({
+				message: "invalid"
+			})
+		}
+	});
+})
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+})
 
 // io socket listener for on connenction
 io.on('connection', function(socket){
-  console.log('a user connected');
-// listens for response from client side (index.html line 46)
-  socket.on('chat message', function(msg){
-  	io.emit('message', msg);
-  });
-// for when client socket side disconnects
-  socket.on('disconnect', function(){
-  	console.log('user disconnect');
-  });
-});
+	// listens for response from client side (index.html line 46)
+	  socket.on('chat message', function(msg, userId){
+	  	io.emit('message', msg, userId);
+	  });
+	// for when client socket side disconnects
+	  socket.on('disconnect', function(){
+	  	console.log('user disconnect');
+	  });
+ });
 
-
-require('./control.js')(app);
 
 http.listen(3000, function(){
   console.log('listening on :3000');
